36cc7779f3c3ca88fa040f3e65d5bea4a50d8c73
vilmar23 <vilmar_mcb@hotmail.com>
2014-02-13 18:51:33 +0100

    prueba final del dia

--- www/hello.js
+++ www/hello.js
@@ -1,4 +1,4 @@
- window.hello = function(str, callback) {
+function(str, callback) {
         cordova.exec(callback, function(err) {
             callback('Nothing to echo.');
         }, "Hello", "echo", [str]);
