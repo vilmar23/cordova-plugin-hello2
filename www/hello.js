c4853f012a807747db1abdc5711a111cf9b35b06
vilmar23 <vilmar_mcb@hotmail.com>
2014-02-13 18:01:54 +0100

    mas cambios

--- www/hello.js
+++ www/hello.js
@@ -1,13 +1,7 @@
-var exec = require('cordova/exec');
+window.hello = function(str, callback, errorcallback) {
+        cordova.exec(callback, errorcallback, "Hello", "greet", [str]);
+    };
 
-var Hello = function(src, successCallback, errorCallback, statusCallback) {
-
-    this.src = src;
-    this.successCallback = successCallback;
-    this.errorCallback = errorCallback;
-
-    exec(this.successCallback, this.errorCallback, "Hello", "greet", this.src);
-}
 
 
 
